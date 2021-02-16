627ced54ecfdc7091d010d062656924061058337
ono shinji <yjsnpi555@gmail.com>
2021-02-16 15:27:02 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'posts#index'
   resources :posts
-  get :'/', to: 'posts#index'
+  get :'', to: 'posts#index'
   get :guilty, to: 'posts#guilty'
   get :not_guilty, to: 'posts#not_guilty'
   get :little_guilty, to: 'posts#little_guilty'
